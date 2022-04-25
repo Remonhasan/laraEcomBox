69c094d0069545208869f0ccdc1d70c9355e9c74
remonhasanapu <remon.hasan@technovista.com.bd>
2022-04-25 16:56:44 +0600

    fix-image-upload-issues

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -14,6 +14,7 @@ use Brian2694\Toastr\Facades\Toastr;
 use Carbon\Carbon;
 
 
+
 class ProductController extends Controller
 {
     public function index ()
@@ -36,40 +37,47 @@ class ProductController extends Controller
         $inputs = $request->all();
 
         $image = $request->file('image');
-        $slug = str_slug($request->name_en);
-    
+
         if(isset($image))
         {
-            //make unique name for image
-            $currentDate = Carbon::now()->toDateString();
-            $imageName  = $slug.'-'.$currentDate.'-'.uniqid().'.'.$image->getClientOriginalExtension();
-   
+            $productImageName = time() . '-' . $request->name_en . '.' . $request->image->extension();
+
+
             if(!Storage::disk('public')->exists('product'))
             {
                 Storage::disk('public')->makeDirectory('product');
             }
-   
-            $productImage = Image::make($image)->resize(1600,1066)->stream();
-            Storage::disk('public')->put('product/'.$imageName,$productImage);
+
+            $request->image->move(public_path('product', $productImageName));
+
    
         } else {
-            $imageName = "default.png";
+            $productImageName = "default.png";
         }
 
-        $product = new Product();
-        $product->category_id = $request->category_id;
-        $product->subcategory_id = $request->subcategory_id;
-        $product->name_en = $request->name_en;
-        $product->name_bn = $request->name_en;
-        $product->description = $request->description;
-        $product->price = $request->price;
-        $product->is_active = $request->is_active;
-        $product->is_stock = $request->is_stock;
-        $product->price_with_discount = $request->price_with_discount;
-        $product->size = $request->size;
-        $product->image = $imageName;
-        $product->save();
-        // Product::create($inputs);
+        // $image = $request->file('image');
+        // $slug = str_slug($request->name_en);
+    
+        // if(isset($image))
+        // {
+        //     //make unique name for image
+        //     $currentDate = Carbon::now()->toDateString();
+        //     $imageName  = $slug.'-'.$currentDate.'-'.uniqid().'.'.$image->getClientOriginalExtension();
+   
+        //     if(!Storage::disk('public')->exists('product'))
+        //     {
+        //         Storage::disk('public')->makeDirectory('product');
+        //     }
+   
+        //     $productImage = Image::make($image)->resize(1600,1066)->stream();
+        //     Storage::disk('public')->put('product/'.$imageName,$productImage);
+   
+        // } else {
+        //     $imageName = "default.png";
+        // }
+
+        $inputs['image'] = $productImageName;
+        Product::create($inputs);
         Toastr::success('Product Created Successfully', 'Ecommerce', ["positionClass" => "toast-top-center"]);
         return redirect()->back();
     }
