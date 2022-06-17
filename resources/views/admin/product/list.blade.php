1a73cb164fac18955b74e2b28c469133c324db8d
remonhasan <remonhasan041@gmail.com>
2022-06-17 15:32:45 +0600

    fix-product-list

--- resources/views/admin/product/list.blade.php
+++ resources/views/admin/product/list.blade.php
@@ -4,96 +4,96 @@
 @endsection
 
 @section('admin_content')
-    <main>
-        <div class="container-fluid px-4">
-            <h1 class="mt-4">{{ __('Products') }}</h1>
-            <ol class="breadcrumb mb-4">
-                <li class="breadcrumb-item">{{ __('Products') }}</li>
-                <li class="breadcrumb-item active"><a href="{{ route('product.list') }}">{{ __('List') }}</a></li>
-            </ol>
-            <div class="card mb-4">
-                <div class="card-body">
-                    DataTables is a third party plugin that is used to generate the demo table below. For more information
-                    about DataTables, please visit the
-                    <a target="_blank" href="https://datatables.net/">official DataTables documentation</a>
-                    .
-                </div>
-            </div>
-            <div class="card mb-4">
-                <div class="card-header">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <i class="fas fa-table mt-2"></i>
-                            DataTable Example
-                        </div>
-                        <div class="col-md-6 d-flex justify-content-end">
-                            <a href="{{ route('product.create') }}" type="button" class="btn btn-primary"><i
-                                    class="fas fa-plus me-1"></i>{{ __('Add') }}</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="card-body">
-                    <table id="datatablesSimple">
-                        <thead>
-                            <tr>
-                                <th>{{ __('SL') }}</th>
-                                <th>{{ __('Name (In English)') }}</th>
-                                <th>{{ __('Name (In Bangla)') }}</th>
-                                <th>{{ __('Image') }}</th>
-                                <th>{{ __('Created at') }}</th>
-                                <th>{{ __('Is Active?') }}</th>
-                                <th>{{ __('Action') }}</th>
-                            </tr>
-                        </thead>
-                        <tfoot>
-                            <tr>
-                                <th>{{ __('SL') }}</th>
-                                <th>{{ __('Name (In English)') }}</th>
-                                <th>{{ __('Name (In Bangla)') }}</th>
-                                <th>{{ __('Image') }}</th>
-                                <th>{{ __('Created at') }}</th>
-                                <th>{{ __('Is Active?') }}</th>
-                                <th>{{ __('Action') }}</th>
-                            </tr>
-                        </tfoot>
-                        <tbody>
-                            @foreach ($allProducts as $key => $product)
-                                <tr>
-                                    <td>{{ $key + 1 }}</td>
-                                    <td>{{ $product->name_en }}</td>
-                                    <td>{{ $product->name_bn }}</td>
-                                    <td>
-                                        <img class="img-responsive thumbnail" src="{{ asset('storage/product/' . $product->image )}}" alt="">
-                                    </td>
-                                    <td>{{ $product->is_active }}</td>
-                                    <td>{{ $product->created_at }}</td>
-                                    <td>
-                                        <span>
-                                            <a href="" type="button" class="btn-sm btn-primary"><i
-                                                    class="fas fa-eye me-1"></i>{{ __('View') }}</a>
-                                        </span>
-                                        <span>
-                                            <a href="{{ route('product.edit', $product->id) }}" type="button"
-                                                class="btn-sm btn-info"><i
-                                                    class="fas fa-pen me-1"></i>{{ __('Edit') }}</a>
-                                        </span>
-                                        <span>
-                                            <form action="{{ route('product.delete', $product->id) }}" method="post">
-                                                @csrf
-                                                @method('delete')
-                                                <button type="submit" class="btn-sm btn-danger"><i
-                                                        class="fas fa-trash me-1"></i>{{ __('Delete') }}</button>
-                                            </form>
-                                        </span>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
+<main>
+    <div class="container-fluid">
+        <h1 class="mt-4">{{ __('Products') }}</h1>
+
+        <div class="row mb-2">
+            <div class="col-md-12">
+                <ol class="breadcrumb shadow">
+                    <i class="fa-solid fa-border-all mt-1 mr-2"></i>
+                    <li class="breadcrumb-item">{{ __('Product') }}</li>
+                    <li class="breadcrumb-item active"><a href="{{ route('product.list') }}">{{ __('List') }}</a></li>
+                    <li class="pull-right">
+                        <a href="{{ route('product.create') }}" type="button" class="btn-sm btn-primary"><i class="fas fa-plus me-1"></i>{{ __('Add') }}</a>
+
+                    </li>
+                </ol>
             </div>
         </div>
-    </main>
+
+        @if (!$allProducts ->isEmpty())
+        <div class="card mb-4">
+            <table class="table table-hover table-striped mb-0">
+                <thead>
+                    <tr class="bg-secondary text-white">
+                        <th>{{ __('SL') }}</th>
+                        <th>{{ __('Name (In English)') }}</th>
+                        <th>{{ __('Name (In Bangla)') }}</th>
+                        <th>{{ __('Is Active?') }}</th>
+                        <th>{{ __('Created at') }}</th>
+                        <th>{{ __('Action') }}</th>
+                    </tr>
+                </thead>
+                <tfoot>
+                    <tr class="bg-secondary text-white">
+                        <th>{{ __('SL') }}</th>
+                        <th>{{ __('Name (In English)') }}</th>
+                        <th>{{ __('Name (In Bangla)') }}</th>
+                        <th>{{ __('Is Active?') }}</th>
+                        <th>{{ __('Created at') }}</th>
+                        <th>{{ __('Action') }}</th>
+                    </tr>
+                </tfoot>
+                <tbody>
+                    @foreach ($allProducts as $key => $product)
+                    <tr>
+                        <td>{{ $key + 1 }}</td>
+                        <td>{{ $product->name_en }}</td>
+                        <td>{{ $product->name_bn }}</td>
+                        <td>
+                            <img class="img-responsive thumbnail" src="{{ asset('storage/product/' . $product->image )}}" alt="">
+                        </td>
+                        <td>{{ $product->is_active }}</td>
+                        <td>{{ $product->created_at }}</td>
+                        <td>
+                            <div class="dropdown">
+                                <button class="btn btn-sm btn-outline-secondary dropdown-toggle" type="button" id="row-action-button-1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    {{ __('Actions') }}
+                                </button>
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="row-action-button-1">
+                                    <a href="" class="dropdown-item">
+                                        <i class="fa-solid fa-eye"></i> {{ __('View') }}
+                                    </a>
+                                    <div class="dropdown-divider"></div>
+                                    <a href="{{ route('product.edit', $product->id) }}" class="dropdown-item">
+                                        <i class="fa-solid fa-pen-to-square"></i> {{ __('Edit') }}
+                                    </a>
+                                    <div class="dropdown-divider"></div>
+                                    <form action="{{ route('product.delete', $product->id) }}" method="POST">
+                                        <button type="submit" onclick="return confirm('Are you sure?')" class="delete-user btn btn-link text-danger dropdown-item">
+                                            <i class="fa-solid fa-trash"></i> {{ __('Delete') }}
+                                        </button>
+                                        @csrf
+                                        @method('delete')
+                                    </form>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+
+        {!! gridFooter($allProducts, $itemsPerPage) !!}
+        @else
+        <div class="alert alert-info alert-styled-left" role="alert">
+            {{ __('Sorry! No data found to display') }}
+        </div>
+        @endif
+    </div>
+</main>
 @endsection
 
 @section('scripts')
